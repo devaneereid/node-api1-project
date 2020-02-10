86891dc51e3008bf5720e207c199a8933ae7284a
Devanee Reid <devaneereid@gmail.com>
2020-02-10 14:27:53 -0700

    get request

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+// GET Requests
+server.get('/', (req, res) => {
+    res.send({ api: "API Working" })
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+      .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+    });
+});
